f86a7cbbcdb070e8b34a6d9937f249bf0881e4a4
charliecharlie <jeancharles552@gmail.com>
2024-04-21 16:24:31 -0300

    chore: remove old app

--- bot_consumer/consumer/worker.js
+++
@@ -1,7 +0,0 @@
-const { workerData } = require('worker_threads');
-const handler = require('../handlers/handler');
-
-const payload = workerData;
-
-// Perform message handling using the handler module
-handler.handleMessage(payload);
