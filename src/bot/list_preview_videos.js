ff5286cca272f1ce0b051250c867346dedd75c36
charliecharlie <jeancharles552@gmail.com>
2024-05-02 12:08:52 -0300

    feat: added unit tests with node:test

--- src/bot/list_preview_videos.js
+++ src/bot/list_preview_videos.js
@@ -1,6 +1,30 @@
-import { readdirSync } from 'fs';
+import { readdirSync as fsReaddirSync } from 'node:fs';
 
-function listPreviewVideos(directoryPath) {
+/**
+ * Lists all preview videos from a directory.
+ * A preview video is a video that has "\_compressed" in it's name.
+ *
+ * @param {string} directoryPath - The directory where the preview files are.
+ * @param {function} readdirSync - The callback to read the directory. default is the readdirSync from "node:fs". Also used to mock unit tests.
+ *
+ * @returns {string[]} An string array with the path of the compressed files.
+ *
+ * @example
+ * <caption>Listing compressed files from a folder.</caption>
+ * listPreviewVideos("./videos/");
+ * ["./videos/video1_compressed.mp4", "./videos/video2_compressed.mp4"]
+ *
+ * @example
+ * <caption>Listing a folder that doesn't have any compressed files.</caption>
+ * listPreviewVideos("./not_compressed/");
+ * []
+ *
+ * @example
+ * <caption>Listing unexistent path/directory.</caption>
+ * listPreviewVideos("/path/to/unexistent/directory/");
+ * []
+ */
+function listPreviewVideos(directoryPath, readdirSync = fsReaddirSync) {
   try {
     const files = readdirSync(directoryPath);
 
---
+++ test/bot/list_preview_videos.test.js
@@ -0,0 +1,54 @@
+import listPreviewVideos from '../../src/bot/list_preview_videos.js';
+import { describe, it, mock } from "node:test";
+import assert from "node:assert";
+
+describe('listPreviewVideos', () => {
+  it('should list compressed mp4 files in the directory', () => {
+    const directoryPath = '/path/to/directory/';
+    const mockFiles = ['video1_compressed.mp4', 'video2.mp4', 'video3_compressed.mp4'];
+
+    // Mock readdirSync to return mockFiles
+    const readdirSync = mock.fn(_path => {
+      return mockFiles;
+    })
+
+    const result = listPreviewVideos(directoryPath, readdirSync);
+
+    const expectedFiles = [
+      '/path/to/directory/video1_compressed.mp4',
+      '/path/to/directory/video3_compressed.mp4',
+    ];
+
+    assert.strictEqual(readdirSync.mock.callCount(), 1)
+    assert.deepEqual(expectedFiles, result);
+  });
+
+  it('should return an empty array if no compressed mp4 files are found', () => {
+    const directoryPath = '/path/to/directory';
+    const mockFiles = ['video1.mp4', 'video2.mp4', 'video3.mov'];
+
+    // Mock readdirSync to return mockFiles
+    const readdirSync = mock.fn(_path => {
+      return mockFiles;
+    })
+
+    const result = listPreviewVideos(directoryPath, readdirSync);
+
+    assert.deepEqual(result, []);
+    assert(readdirSync.mock.callCount(), 1);
+  });
+
+  it('should return an empty array if an error occurs during file reading', () => {
+    const directoryPath = '/path/to/directory';
+
+    // Mock readdirSync to throw an error
+    const readdirSync = mock.fn(_path => {
+      throw new Error('Test error');
+    })
+
+    const result = listPreviewVideos(directoryPath, readdirSync);
+
+    assert.deepEqual(result, []);
+    assert(readdirSync.mock.callCount(), 1);
+  });
+});
